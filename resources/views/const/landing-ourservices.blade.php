a8309b808150cb18738cd9c7336f857a6d1301c3
tripletens <tripletens.kc@gmail.com>
2021-11-30 16:27:21 +0100

    I added the employers and the job seekers procedure

--- resources/views/const/landing-ourservices.blade.php
+++ resources/views/const/landing-ourservices.blade.php
@@ -1,11 +1,13 @@
 <section class="our-services">
             <div class="container">
-                <div class="row">
+                <div class="row" >
                     <div class="col-md-7">
                         <div class="left-content">
                             <br>
-                            <h4>About us</h4>
-                            <p>Aenean hendrerit metus leo, quis viverra purus condimentum nec. Pellentesque a sem semper, lobortis mauris non, varius urna. Quisque sodales purus eu tellus fringilla.<br><br>Mauris sit amet quam congue, pulvinar urna et, congue diam. Suspendisse eu lorem massa. Integer sit amet posuere tellus, id efficitur leo. In hac habitasse platea dictumst. Vel sequi odit similique repudiandae ipsum iste, quidem tenetur id impedit, eaque et, aliquam quod.</p>
+                            <h4>WELCOME TO KANSELOR VACANCY <br/> where Jobs are created for unemployed graduates and Businesses are made more profitable for Nigerian business owner</h4>
+                            <p>
+                            At we work with Business owners to create jobs for unemployed graduates and we prepare unemployed Nigerian graduates to make businesses more profitable when business owners employ them
+                            </p>
                             <div class="blue-button">
                                 <a href="{{route('about-page')}}">Discover More</a>
                             </div>
@@ -14,6 +16,68 @@
                     </div>
                     <div class="col-md-5">
                         <img src="{{asset('img/about-1-720x480.jpg')}}" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <br/><br/><br/>
+                <hr/>
+                <h2 class="text-center my-7"> Our Services </h2> <br/><br/>
+                <div class="row d-flex justify-content-center my-5" >
+                    <div class="col-md-6">
+                        <div class="card" style=" height:90vh;padding:20px;border-radius:5px;box-shadow: 2px 2px 8px 0px rgba(128,128,128,0.75);-webkit-box-shadow: 2px 2px 8px 0px rgba(128,128,128,0.75);-moz-box-shadow: 2px 2px 8px 0px rgba(128,128,128,0.75);">
+                            
+                            <h3 class="text-center"> KANSELOR FOR EMPLOYERS </h3>
+                            <p>We help Nigerian employers sources very good hands. Then prepare these staff to deliver excellent results in the job. We also help the fire underperforming staffs</p>
+                            <br/>
+                            <h5>HOW IT WORKS FOR EMPLOYERS</h5>
+                            <ul type="solid">
+                                <li class="m-3"> STEP ONE - Download the Kanselor app</li><br>
+                                <li>STEP TWO - Fill out your business information</li> <br>
+                                <li>STEP THREE - List the Vacancies you want to fill</li> <br>
+                                <li>STEP FOUR - Make the commitment</li><br>
+                                <li>STEP FIVE - Listen to audio of pre-interviewed candidates</li><br>
+                                <li>STEP SIX - Pick a date to meet the Candidates</li><br>
+                                <li>STEP SEVEN - Interview the candidates</li><br>
+                                <li>STEP EIGHT - Close the opening</li><br>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card shadow" style="height:90vh;padding:20px;border-radius:5px;box-shadow: 2px 2px 8px 0px rgba(128,128,128,0.75);-webkit-box-shadow: 2px 2px 8px 0px rgba(128,128,128,0.75);-moz-box-shadow: 2px 2px 8px 0px rgba(128,128,128,0.75);">
+                            
+                            <h3 class="text-center"> KANSELOR FOR NIGERIAN JOB SEEKERS </h3>
+                            <p>We prepare Nigerian graduates to be better equipped to land good jobs, we help them source for meaningful jobs and orientate them to perform excellently well on the job.
+                            <br/>
+                                We also help relocate them when a job is not the right fit for them</p>
+                            <br/>
+                            <h5>HOW IT WORKS FOR EMPLOYERS</h5>
+                            <ul type="solid">
+                                <li class="m-3"> STEP ONE - Download the Kanselor app</li><br>
+                                <li>STEP TWO - Fill in the CV template on the app</li> <br>
+                                <li>STEP THREE - Choose from a list of jobs that come up</li> <br>
+                                <li>STEP FOUR - Take a mock interview</li><br>
+                                <li>STEP FIVE - Get a performance score and areas of improvement</li><br>
+                                <li>STEP SIX - Take free courses to improve performance</li><br>
+                                <li>STEP SEVEN -  Attend a virtual interview for a Job</li><br>
+                                <li>STEP EIGHT - Attend a real interview with an employer</li><br>
+                                <li>STEP NINE - Start the job</li><br>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+
+        <section style="background:#f8f8f8;">
+            <div class="container">
+                <div class="row d-flex justify-content-center my-5" >
+                    <div class="col-md-12">
+                        <div class="card shadow" style="height:50vh;padding:20px;border-radius:5px;box-shadow: 2px 2px 8px 0px rgba(128,128,128,0.75);-webkit-box-shadow: 2px 2px 8px 0px rgba(128,128,128,0.75);-moz-box-shadow: 2px 2px 8px 0px rgba(128,128,128,0.75);">
+                            <h3 class="text-center" style="margin:10vh;margin-bottom:20px;">THE KANSELOR BUSINESS MASTERCLASS</h3>
+                            <p class="text-center" style="font-size:20px;margin:10vh; margin-top:0px;">This is a weekly Business and Work improvement classes designed to 10X the current results and income you are generating.
+                                <!-- <br/> -->
+                            It's a practical hands-on session that equip both the job seeker and business owners with business improvement skills</p>
+                        </div>
                     </div>
                 </div>
             </div>
