01d6d843bbeff22e353a3c26fa62009c23e5c4c2
tripletens <tripletens.kc@gmail.com>
2021-12-10 12:32:49 +0100

    added the youtube url instead of the video its self

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -87,15 +87,7 @@
             <div class="video-overlay"></div>
             <div class="video-content">
                 <div class="inner">
-                    <video width="320" height="240" controls>
-
-                        <source src="{{asset('video/video.mp4')}}" type="video/mp4">
-
-                        <source src="movie.ogg" type="video/ogg">
-
-                        Your browser does not support the video tag.
-
-                    </video>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/HMQlA-TIAsk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                       <!-- <div class="section-heading">
                           <span>Contact Us</span>
                           <h2>Let get in touch</h2>
