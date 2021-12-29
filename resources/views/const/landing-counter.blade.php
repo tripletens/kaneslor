6e90b92f349dfd6a8a8949b2f90e8ef52b56c9d0
tripletens <tripletens.kc@gmail.com>
2021-12-29 23:15:24 +0100

    started the counter

--- resources/views/const/landing-counter.blade.php
+++ resources/views/const/landing-counter.blade.php
@@ -2,20 +2,24 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-sm-12 col-xs-12">
-                <div class="featured-item">
-
+                <div class="featured-item counter-div">
+                    <h4>No of Companies</h4>
                 </div>
             </div>
             <div class="col-md-4 col-sm-12 col-xs-12">
                 <div class="featured-item">
-
+                    <h4>Number of Jobseekers</h4>
                 </div>
             </div>
             <div class="col-md-4 col-sm-12 col-xs-12">
                 <div class="featured-item">
-
+                    <h4>Number of jobs Secured</h4>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+<style type="text/css">
+
+</style>
