e23f13c10a6b76bb0880ca3cf2f58d8abbcfe7c4
tripletens <tripletens.kc@gmail.com>
2021-12-29 23:47:51 +0100

    added text align center

--- resources/views/const/landing-counter.blade.php
+++ resources/views/const/landing-counter.blade.php
@@ -1,19 +1,22 @@
 <section class="featured-places">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-sm-12 col-xs-12">
-                <div class="featured-item counter-div mx-auto">
-                    <h4 >No of Companies</h4>
+    <div class="container-fluid mt-5 ">
+        <div class="row mt-5 mb-0">
+            <div class="col-md-4 col-sm-12 col-xs-12 d-flex justify-content-center">
+                <div class="featured-item counter-div mx-auto  ">
+                    <h4 class="text-center">No of Companies</h4>
+                    <p class="text-center"> 60</p>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-12 col-xs-12">
-                <div class="featured-item mx-auto">
+            <div class="col-md-4 col-sm-12 col-xs-12 d-flex justify-content-center">
+                <div class="featured-item mx-auto ">
                     <h4>Number of Jobseekers</h4>
+                    <p class="text-center"> 2300 </p>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-12 col-xs-12">
-                <div class="featured-item mx-auto">
+            <div class="col-md-4 col-sm-12 col-xs-12 d-flex justify-content-center">
+                <div class="featured-item mx-auto  ">
                     <h4>Number of jobs Secured</h4>
+                    <p class="text-center">1000</p>
                 </div>
             </div>
         </div>
