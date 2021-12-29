71aa4764a6b6746a6c0778d52a9bc8b82e681afd
tripletens <tripletens.kc@gmail.com>
2021-12-29 16:25:36 +0100

    added the background image

---
+++ resources/views/const/landing-counter.blade.php
@@ -0,0 +1,21 @@
+<section class="featured-places">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-12 col-xs-12">
+                <div class="featured-item">
+
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12">
+                <div class="featured-item">
+
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12">
+                <div class="featured-item">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
