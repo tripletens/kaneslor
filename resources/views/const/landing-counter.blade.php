c62fec3a8cf1d1f84505c632aebe663b8c3fbdb7
tripletens <tripletens.kc@gmail.com>
2021-12-31 11:23:09 +0100

    added the counter

--- resources/views/const/landing-counter.blade.php
+++ resources/views/const/landing-counter.blade.php
@@ -4,13 +4,13 @@
             <div class="col-md-4 col-sm-12 col-xs-12 d-flex justify-content-center">
                 <div class="featured-item counter-div mx-auto  ">
                     <h4 class="text-center">No of Companies</h4>
-                    <p class="text-center"> 60</p>
+                    <p class="text-center counter"> 60</p>
                 </div>
             </div>
             <div class="col-md-4 col-sm-12 col-xs-12 d-flex justify-content-center">
                 <div class="featured-item mx-auto ">
                     <h4>Number of Jobseekers</h4>
-                    <p class="text-center"> 2300 </p>
+                    <p class="text-center "> 2300 </p>
                 </div>
             </div>
             <div class="col-md-4 col-sm-12 col-xs-12 d-flex justify-content-center">
@@ -23,6 +23,25 @@
     </div>
 </section>
 
-<style type="text/css">
-
+<style type="text/css" rel="stylesheet">
+    .count {
+        width: 60px;
+        height: 60px;
+        background: lightgray;
+        border-radius: 50%;
+        border: 1px solid gray;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: red;
+        font-weight: bold;
+        font-size: 20px;
+        font-family: verdana;
+        float: left;
+        margin: 30px;
+        text-shadow: 1px 1px black;
+        -webkit-box-shadow: 0px 0px 72px -9px rgba(0,0,0,0.75);
+        -moz-box-shadow: 0px 0px 72px -9px rgba(0,0,0,0.75);
+        box-shadow: 0px 0px 72px -9px rgba(0,0,0,0.75);
+    }
 </style>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,32 +1,80 @@
-
 @extends('layouts.landing')
 
 @section('content')
-          
+
 <section class="banner" id="top" style="background-image: url({{asset('img/Kanselor.jfif')}});">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                    <div class="banner-caption">
-                        <div class="line-dec"></div>
-                        <h2 style="font-weight:bolder;">PICK YOUR DREAM JOB AND KIYIX FIND IT FOR YOU </h2>
-                        <div class="row" style="width:50%;">
-                            <div class="col-md-6">
-                                <div class="blue-button">
-                                    <a href="{{route('register')}}">Register as an Employer</a>
-                                </div> 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="banner-caption">
+                    <div class="line-dec"></div>
+                    <h2 style="font-weight:bolder;">PICK YOUR DREAM JOB AND KIYIX FIND IT FOR YOU </h2>
+                    <div class="row" style="width:50%;">
+                        <div class="col-md-6">
+                            <div class="blue-button">
+                                <a href="{{route('register')}}">Register as an Employer</a>
                             </div>
-                            <div class="col-md-6">
-                                <div class="blue-button">
-                                    <a href="{{route('register')}}">Register as an Applicant</a>
-                                </div> 
+                        </div>
+                        <div class="col-md-6">
+                            <div class="blue-button">
+                                <a href="{{route('register')}}">Register as an Applicant</a>
                             </div>
-                            <!-- <div class="blue-button">
+                        </div>
+                        <!-- <div class="blue-button">
                                 <a href="{{route('contact-page')}}">Register as an Employer</a>
                             </div> -->
-                            <!-- <div class="blue-button">
+                        <!-- <div class="blue-button">
                                 <a href="{{route('contact-page')}}">Register as an Applicant </a>
                             </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<main>
+    @include('const.landing-ourservices')
+
+    <section class="featured-places">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-heading">
+                        <span>Why Choose us</span>
+                        <h2> We Provide Jobs to over 10 million Nigerians</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="featured-item">
+                        <div class="down-content" style="height:200px;">
+
+                            <h2 class="text-center my-3 mx-5">Employers </h2>
+
+                            <p class="text-center">As an employer, we provide you with immediate access to top-quality candidates</p>
+
+                            <div class="blue-button" style="margin-bottom:10px;display:flex; flex-direction:row; align:center;justify-content:center;">
+                                <a href="{{route('register')}}">Get Started</a>
+                            </div>
+                            <br />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="featured-item">
+
+                        <div class="down-content" style="height:200px;">
+                            <h2 class="text-center my-3">Job Seeker</h2>
+
+                            <p class="text-center mt-4">We make it easier to get jobs that suits you</p>
+
+                            <div class="blue-button mt-5" style="margin-bottom:10px;display:flex; flex-direction:row; align:center;justify-content:center;">
+                                <a href="{{route('register')}}">Get Started</a>
+                            </div>
+                            <br />
                         </div>
                     </div>
                 </div>
@@ -34,76 +82,51 @@
         </div>
     </section>
 
-    <main>
-        @include('const.landing-ourservices')
-
-        <section class="featured-places">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="section-heading">
-                            <span>Why Choose us</span>
-                            <h2> We Provide Jobs to over 10 million Nigerians</h2>
-                        </div>
-                    </div> 
-                </div> 
-                <div class="row">
-                    <div class="col-md-6 col-sm-6 col-xs-12">
-                        <div class="featured-item">
-                            <div class="down-content"  style="height:200px;">
-
-                                <h2 class="text-center my-3 mx-5">Employers </h2>
-
-                                <p class="text-center">As an employer, we provide you with immediate access to top-quality candidates</p>
-
-                                <div class="blue-button" style="margin-bottom:10px;display:flex; flex-direction:row; align:center;justify-content:center;">
-                                    <a href="{{route('register')}}">Get Started</a>
-                                </div>
-                                <br/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6 col-sm-6 col-xs-12">
-                        <div class="featured-item" >
-
-                            <div class="down-content" style="height:200px;">
-                                <h2 class="text-center my-3">Job Seeker</h2>
-
-                                <p class="text-center mt-4">We make it easier to get jobs that suits you</p>
-
-                                <div class="blue-button mt-5" style="margin-bottom:10px;display:flex; flex-direction:row; align:center;justify-content:center;">
-                                    <a href="{{route('register')}}">Get Started</a>
-                                </div>
-                                <br/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section id="video-container">
-            <div class="video-overlay"></div>
-            <div class="video-content">
-                <div class="inner">
+    <section id="video-container">
+        <div class="video-overlay"></div>
+        <div class="video-content">
+            <div class="inner">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/G8W5hAdSMkc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                      <!-- <div class="section-heading">
+                <!-- <div class="section-heading">
                           <span>Contact Us</span>
                           <h2>Let get in touch</h2>
                       </div> -->
-                      <!-- Modal button -->
+                <!-- Modal button -->
 
-                      <!-- <div class="blue-button">
+                <!-- <div class="blue-button">
                         <a href="{{route('contact-page')}}">Talk to us</a>
                       </div> -->
+            </div>
+        </div>
+    </section>
+
+
+    <section class="featured-places">
+        <div class="container-fluid mt-5 ">
+            <div class="row mt-5 mb-0">
+                <div class="col-md-4 col-sm-12 col-xs-12 d-flex justify-content-center">
+                    <div class="featured-item counter-div mx-auto  ">
+                        <h2 class="text-center ">No of Companies</h2>
+                        <p class="text-center counter-no" > 60</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12 col-xs-12 d-flex justify-content-center">
+                    <div class="featured-item mx-auto ">
+                        <h2>Number of Jobseekers</h2>
+                        <p class="text-center  counter-no"> 2300 </p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12 col-xs-12 d-flex justify-content-center">
+                    <div class="featured-item mx-auto  ">
+                        <h2>Number of Jobs Secured</h2>
+                        <p class="text-center counter-no">1000</p>
+                    </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
 
-        @include('const.landing-counter')
-
-        @include('const.landing-testimonials')
-        @include('const.landing-map')
-    </main>
+    @include('const.landing-testimonials')
+    @include('const.landing-map')
+</main>
 @endsection
